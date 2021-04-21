05cd26d244d2802bfc2d67205e73bb6a722f5c2f
Christopher-Fick <62448565+Christopher-Fick@users.noreply.github.com>
2021-04-21 01:26:57 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -8,7 +8,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = ‘Aging PMC Corrections documents'
+project = ‘STAGED Aging PMC Corrections documents'
  author = 'Aginh'
 
 
